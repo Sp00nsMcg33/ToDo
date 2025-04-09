0f01187935d02a0095fc16edf911275b5b6c2351
Sp00nsMcg33 <thomasclarke6@gmail.com>
2025-04-09 11:04:24 +0100

    Created links to custom SASS files custom.sass.

---
+++ ToDo/src/app/task-list/task-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { TaskComponent } from '../task/task.component';
+
+@Component({
+  selector: 'app-task-list',
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css'
+})
+export class TaskListComponent {
+
+}
